ee6a8aa971725fe9bcd8ab3b87436dc2e9aed52f
Laurent Dupuis <1aurent@users.noreply.github.com>
2015-03-04 10:27:54 +0000

    Fixes

--- WiredTigerTest/Program.cs
+++ WiredTigerTest/Program.cs
@@ -13,17 +13,18 @@ namespace WiredTigerTest
             using(var tst = WiredTigerNet.Connection.Open(@"C:\_perso\test\DB\", "create"))
             using(var cnx = tst.OpenSession(""))
             {
-                cnx.Create("table:test", "key_format=q,value_format=u");
+                cnx.Create("table:test", "key_format=q,value_format=u,block_compressor=snappy");
                 using(var crc = cnx.OpenCursorLK("table:test"))
                 {
-                    crc.SetKey(10);
-                    crc.SetValue(Encoding.ASCII.GetBytes("Hello"));
-                    crc.Insert();
+                    for (var i = 1000000; i >= 0; --i)
+                    {
+                        if ((i % 1000) == 0) Console.Write("\r{0}", i);
+                        crc.SetKey(i);
+                        crc.SetValue(Encoding.ASCII.GetBytes("Hello world "+ i));
+                        crc.Insert();
+                    }
 
-                    crc.SetKey(11);
-                    crc.SetValue(Encoding.ASCII.GetBytes("World"));
-                    crc.Insert();
-
+                    Console.WriteLine(" ... Completed");
                     crc.Reset();
                     while(crc.Next())
                     {
