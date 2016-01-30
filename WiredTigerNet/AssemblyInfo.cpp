59f378d7b033c6f51837700a5a41f3d495d95cc0
Laurent Dupuis <1aurent@users.noreply.github.com>
2016-01-30 03:40:40 +0000

    Add legal

--- WiredTigerNet/AssemblyInfo.cpp
+++ WiredTigerNet/AssemblyInfo.cpp
@@ -1,3 +1,22 @@
+/* ........................................................................
+* copyright 2015-2016 Laurent Dupuis
+* ........................................................................
+* < This program is free software: you can redistribute it and/or modify
+* < it under the terms of the GNU General Public License as published by
+* < the Free Software Foundation, either version 3 of the License, or
+* < (at your option) any later version.
+* <
+* < This program is distributed in the hope that it will be useful,
+* < but WITHOUT ANY WARRANTY; without even the implied warranty of
+* < MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* < GNU General Public License for more details.
+* <
+* < You should have received a copy of the GNU General Public License
+* < along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ........................................................................
+*
+*/
+
 #include "stdafx.h"
 
 using namespace System;
@@ -12,11 +31,11 @@ using namespace System::Security::Permissions;
 // associated with an assembly.
 //
 [assembly:AssemblyTitleAttribute(L"WiredTigerNet")];
-[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyDescriptionAttribute(L"WiredTiger database for .NET")];
 [assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"Laurent Dupuis")];
 [assembly:AssemblyProductAttribute(L"WiredTigerNet")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2015")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright 2015-2016 Laurent Dupuis")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
@@ -31,7 +50,7 @@ using namespace System::Security::Permissions;
 // You can specify all the value or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:AssemblyVersionAttribute("2.7.0")];
 
 [assembly:ComVisible(false)];
 
--- WiredTigerNet/Stdafx.h
+++ WiredTigerNet/Stdafx.h
@@ -1,11 +1,24 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently,
-// but are changed infrequently
-
+/* ........................................................................
+* copyright 2015-2016 Laurent Dupuis
+* ........................................................................
+* < This program is free software: you can redistribute it and/or modify
+* < it under the terms of the GNU General Public License as published by
+* < the Free Software Foundation, either version 3 of the License, or
+* < (at your option) any later version.
+* <
+* < This program is distributed in the hope that it will be useful,
+* < but WITHOUT ANY WARRANTY; without even the implied warranty of
+* < MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* < GNU General Public License for more details.
+* <
+* < You should have received a copy of the GNU General Public License
+* < along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ........................................................................
+*
+*/
 #pragma once
 
 #include <string.h>
+#include <wiredtiger.h>
 
 #pragma comment(lib,"WiredTiger.lib")
-
-#include <wiredtiger.h>
